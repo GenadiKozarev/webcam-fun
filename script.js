e8e0beec186604656b23b20fc973c5445d62aafb
gkoz <genadi.kozarev+github@gmail.com>
2024-11-21 19:07:34 +0000

    adds 'green screen' effect taking certain pixels out of the canvas using sliders for each channel

--- script.js
+++ script.js
@@ -40,9 +40,10 @@ const paintToCanvas = () => {
         ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
         // Extract pixel data from canvas
         let pixels = ctx.getImageData(0, 0, videoWidth, videoHeight);
-         // Apply color manipulation effect to pixels
+        // Apply color manipulation effect to pixels
         // pixels = sunnyEffect(pixels);
         // pixels = psychedelicEffect(pixels);
+        // pixels = greenScreen(pixels);
         // Ghost effect
         // ctx.globalAlpha = 0.1;
         // Render modified pixels back to canvas
@@ -83,6 +84,30 @@ const psychedelicEffect = pixels => {
         pixels.data[i + 108] = pixels.data[i + 0]; // Red
         pixels.data[i + 86] = pixels.data[i + 1]; // Green
         pixels.data[i - 20] = pixels.data[i + 2]; // Blue
+    }
+    return pixels;
+};
+
+const greenScreen = pixels => {
+    const levels = {};
+    document.querySelectorAll('.rgb input').forEach(input => {
+        levels[input.name] = input.value;
+    });
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+        if (
+            red >= levels.rmin &&
+            green >= levels.gmin &&
+            blue >= levels.bmin &&
+            red <= levels.rmax &&
+            green <= levels.gmax &&
+            blue <= levels.bmax
+        ) {
+            pixels.data[i + 3] = 0;
+        }
     }
     return pixels;
 };
